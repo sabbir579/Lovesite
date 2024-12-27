7dd1c8309564cf9995ec1eef431f0291d24b6c2b
sabbir579 <rsabbirhossain.official@gmail.com>
2024-12-27 17:18:17 +0600

    LoveSite

---
+++ apps.js
@@ -0,0 +1,10 @@
+function LoveSite(){
+    var contain=document.getElementById("contain");
+    contain.innerHTML=" "
+    const myAudio=document.getElementById("myAudio");
+    myAudio.play();
+    var h2mother=document.createElement("h2");
+    h2mother.innerText=("🩷💚 হাজার অশান্তির মাঝে তুমি আমার একমাত্র শান্তির জায়গা!🫰🌹🫶")
+
+    contain.appendChild(h2mother)
+}
